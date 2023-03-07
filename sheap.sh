d0b8b40f9c52883fe11be0eb2ff6d562226441f9
Jakob Kaivo <jkk@ung.org>
2023-03-07 10:12:44 -0500

    modify script to directly take .c files instead of manually creating and passing .ll files

--- sheap.sh
+++ sheap.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-opt -enable-new-pm=0 -load "$(dirname $0)/Sheap.so" -sheap "$@"
+
+while [ $# -gt 0 ]; do
+	clang "$1" -S -emit-llvm -o - | opt -enable-new-pm=0 -load "$(dirname $0)/Sheap.so" -sheap
+	shift
+done
