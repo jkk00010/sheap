0a6d4c023fea09e12885e838350457c2aeebfe89
Jakob Kaivo <jkk@ung.org>
2023-03-07 23:37:09 -0500

    experiment with different pass types

--- Sheap.cpp
+++ Sheap.cpp
@@ -1,16 +1,21 @@
 #include "llvm/Pass.h"
+#include "llvm/IR/Module.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Analysis/CallGraphSCCPass.h"
+#include "llvm/Analysis/CallGraph.h"
 
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 
 using namespace llvm;
 
-struct Sheap : public FunctionPass {
+struct Sheap : public CallGraphSCCPass {
 	static char ID;
-	Sheap() : FunctionPass(ID) {}
-	bool runOnFunction(Function &F) override;
+	Sheap() : CallGraphSCCPass(ID) {}
+	bool runOnFunction(Function &F); // override;
+	bool runOnModule(Module &M); // override;
+	bool runOnSCC(CallGraphSCC &C) override;
 };
 
 char Sheap::ID = 0;
@@ -19,5 +24,17 @@ static RegisterPass<Sheap> X("sheap", "Sheap Pass");
 bool Sheap::runOnFunction(Function &F) {
 	errs() << "Sheap: ";
 	errs().write_escaped(F.getName()) << '\n';
+	return false;
+}
+
+bool Sheap::runOnModule(Module &M) {
+	errs() << "Sheap: ";
+	errs().write_escaped(M.getName()) << '\n';
+	return false;
+}
+
+bool Sheap::runOnSCC(CallGraphSCC &C) {
+	errs() << "Sheap: ";
+	C.getCallGraph().print(errs());
 	return false;
 }
