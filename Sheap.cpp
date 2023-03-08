7f10279bea93c28e4bca304561156ae368727c4f
Jakob Kaivo <jkk00010@mix.wvu.edu>
2023-03-08 17:26:23 -0500

    remove cruft, flatten remaining code, now identifes heap calls

--- Sheap.cpp
+++ Sheap.cpp
@@ -1,33 +1,94 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Function.h"
-#include "llvm/Support/raw_ostream.h"
-#include "llvm/Analysis/CallGraphSCCPass.h"
-#include "llvm/Analysis/CallGraph.h"
-
-#include "llvm/IR/LegacyPassManager.h"
-#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "llvm/IR/Instructions.h"
 
 using namespace llvm;
 
 struct Sheap : public ModulePass {
 	static char ID;
 	Sheap() : ModulePass(ID) {}
-	bool runOnFunction(Function &F); // override;
+	bool runOnFunction(Function &F);
+	bool runOnCall(Instruction &I);
+	bool runOnBlock(BasicBlock &B);
+	bool runOnInstruction(Instruction &I);
 	bool runOnModule(Module &M) override;
-	bool runOnSCC(CallGraphSCC &C);// override;
 };
 
 char Sheap::ID = 0;
 static RegisterPass<Sheap> X("sheap", "Sheap Pass");
 
-bool Sheap::runOnFunction(Function &F) {
-	errs() << "Sheap: ";
-	errs().write_escaped(F.getName()) << '\n';
+bool Sheap::runOnCall(Instruction &I)
+{
+	CallInst* ci = dyn_cast<CallInst>(&I);
+	Function *fn = ci->getCalledFunction();
+	StringRef name = fn->getName();
+
+	//errs() << "\t" << name << " : " << ci->getArgOperand(0) << "\n";
+	errs() << "\t" << name << "\n";
+	for (unsigned i = 0; i < ci->arg_size(); i++) {
+		errs() << "\t" << *(ci->getArgOperand(i)) << "\n";
+	}
+
+	if (name == "malloc") {
+		/* taint return value */
+	} else if (name == "realloc") {
+		/* taint return value */
+		/* taint first parameter as possibly free */
+	} else if (name == "calloc") {
+		/* taint return value */
+	} else if (name == "free") {
+		/* taint first parameter as definitely free */
+	} else {
+		Sheap::runOnFunction(*fn);
+	}
+
 	return false;
 }
 
-bool Sheap::runOnModule(Module &M) {
+bool Sheap::runOnInstruction(Instruction &I)
+{
+	switch (I.getOpcode()) {
+	case Instruction::Load:
+		//runOnLoad(I);
+		break;
+	case Instruction::Store:
+		//runOnStore(I);
+		break;
+	case Instruction::Call:
+		runOnCall(I);
+		break;
+	}
+	return false;
+}
+
+bool Sheap::runOnFunction(Function &F)
+{
+	errs() << "Analyzing function " << F.getName() << "\n";
+	int i = 0;
+	for (BasicBlock &B : F) {
+		errs() << "block " << i << "\n";
+		runOnBlock(B);
+		i++;
+	}
+	return false;
+}
+
+bool Sheap::runOnBlock(BasicBlock &B)
+{
+	int j = 0;
+	for (Instruction &I : B) {
+		errs() << j << ":\t";
+		I.print(errs());
+		errs() << "\n";
+		runOnInstruction(I);
+		j++;
+	}
+	return false;
+}
+
+bool Sheap::runOnModule(Module &M)
+{
 	errs() << "Sheap: ";
 	errs().write_escaped(M.getName()) << '\n';
 	auto main = M.getFunction("main");
@@ -35,38 +96,6 @@ bool Sheap::runOnModule(Module &M) {
 		errs() << "main() not found, can't complete analysis\n";
 		return false;
 	}
-	//main->print(errs());
-	int i = 0;
-	//for (auto block = main->front(); block != main->back(); block++) {
-	//for (Function::iterator b = main->begin(), be = main->end(); b != be; b++) {
-	for (BasicBlock &block : *main) {
-		errs() << "block " << i << "\n";
-		//block.print(errs());
-		i++;
-
-		int j = 0;
-		for (Instruction &inst : block) {
-			// Instruction::Load
-			// Instruction::Store
-			if (inst.getOpcode() == Instruction::Call || true) {
-				errs() << "Instruction " << j << "\n";
-				errs() << inst << "\n";
-				errs() << inst.getOpcode() << "\n";
-				errs() << inst.getOpcodeName() << "\n";
-			}
-			j++;
-		}
-	}
-	return false;
-}
-
-bool Sheap::runOnSCC(CallGraphSCC &C) {
-	errs() << "Sheap: ";
-	//auto cg = CallGraph(C.getCallGraph());
-	//C.getCallGraph().print(errs());
-	for (auto i = C.begin(); i < C.end(); i++) {
-		//errs() << i << "\n";
-		errs() << "iterating\n";
-	}
+	runOnFunction(*main);
 	return false;
 }
