52240291afe4e54e8e6e9a7c7d4b8a741f04519f
Jakob Kaivo <jkk@ung.org>
2023-03-12 13:44:20 -0400

    include debugging information

--- sheap.sh
+++ sheap.sh
@@ -3,6 +3,6 @@
 LLVM_VERSION="-13"
 
 while [ $# -gt 0 ]; do
-	"clang${LLVM_VERSION}" "$1" -S -emit-llvm -o - | "opt${LLVM_VERSION}" -enable-new-pm=0 -load "$(dirname $0)/Sheap.so" -sheap
+	"clang${LLVM_VERSION}" "$1" -g -S -emit-llvm -o - | "opt${LLVM_VERSION}" -enable-new-pm=0 -load "$(dirname $0)/Sheap.so" -sheap
 	shift
 done
