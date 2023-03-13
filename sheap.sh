e0b70c042cad2a0ac7d0cc850f49f5f844754f07
Jakob Kaivo <jkk@ung.org>
2023-03-12 21:41:13 -0400

    remove -f

--- sheap.sh
+++ sheap.sh
@@ -6,6 +6,6 @@ LLVM_VERSION=
 tput reset
 
 while [ $# -gt 0 ]; do
-	"${LLVM_PATH}clang${LLVM_VERSION}" "$1" -g -S -emit-llvm -o - | "${LLVM_PATH}opt${LLVM_VERSION}" -enable-new-pm=0 -f -load "$(dirname $0)/Sheap.so" -sheap
+	"${LLVM_PATH}clang${LLVM_VERSION}" "$1" -g -S -emit-llvm -o - | "${LLVM_PATH}opt${LLVM_VERSION}" -enable-new-pm=0 -load "$(dirname $0)/Sheap.so" -sheap
 	shift
 done
