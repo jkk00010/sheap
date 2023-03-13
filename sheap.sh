4dd783d5bda51088d835ec9efa7418091ae1e025
Jakob Kaivo <jkk@ung.org>
2023-03-12 21:40:00 -0400

    clear scroll-back before running for easier tracing

--- sheap.sh
+++ sheap.sh
@@ -3,6 +3,8 @@
 LLVM_PATH=/usr/local/bin/
 LLVM_VERSION=
 
+tput reset
+
 while [ $# -gt 0 ]; do
 	"${LLVM_PATH}clang${LLVM_VERSION}" "$1" -g -S -emit-llvm -o - | "${LLVM_PATH}opt${LLVM_VERSION}" -enable-new-pm=0 -f -load "$(dirname $0)/Sheap.so" -sheap
 	shift
